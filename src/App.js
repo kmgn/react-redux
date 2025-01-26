b8a966ae4643f942a9204705a439fb80f2afa397
kmgn <70260168+kmgn@users.noreply.github.com>
2025-01-26 16:52:38 +0530

    added src n public n files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ExpenseEntryItemList from './components/ExpenseEntryItemList';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ExpenseEntryItemList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/actions/index.js
@@ -0,0 +1,21 @@
+// uuid to create unique identifier.
+import { v4 as uuidv4 } from 'uuid';
+import { ADD_EXPENSE, DELETE_EXPENSE } from './types';
+
+export const addExpense = ({ name, amount, spendDate, category }) => ({
+  type: ADD_EXPENSE,
+  payload: {
+    id: uuidv4(),
+    name,
+    amount,
+    spendDate,
+    category
+  }
+});
+
+export const deleteExpense = id => ({
+  type: DELETE_EXPENSE,
+  payload: {
+    id
+  }
+});
